10287cca8b7a7a9e6d2f010e2eca00da5c2e4f1e
William Song <30965609+Freakwill@users.noreply.github.com>
2018-12-07 22:16:00 +0800

    Add files via upload

---
+++ random.js
@@ -0,0 +1,13 @@
+function choice(items) {
+    return items[Math.floor(Math.random()*items.length)];}
+
+function choicex(arr, count) {
+    var shuffled = arr.slice(0), i = arr.length, min = i - count, temp, index;
+    while (i-- > min) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(min);
+}
