d82c55ec7e6cbdf796a52f5a45cc178b14e62d34
William Song <30965609+Freakwill@users.noreply.github.com>
2018-12-08 18:21:32 +0800

    Add files via upload
    add files

--- random.js
+++ random.js
@@ -1,3 +1,4 @@
+
 function choice(items) {
     return items[Math.floor(Math.random()*items.length)];}
 
@@ -10,4 +11,14 @@ function choicex(arr, count) {
         shuffled[i] = temp;
     }
     return shuffled.slice(min);
+}
+
+function randomFromFile(fname) {
+    fs.readFile(fname, function(err, data){
+        if (err)
+            {console.log(err);}
+        else
+            {var output = JSON.parse(data.toString());}
+    })
+    return choice(output)
 }
