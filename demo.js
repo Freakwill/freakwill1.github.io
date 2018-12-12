57299b6ca5f105ce5cd628ac219ed1c97ea263e5
William Song <30965609+Freakwill@users.noreply.github.com>
2018-12-12 15:54:13 +0800

    21th commit

--- demo.js
+++ demo.js
@@ -1,11 +1,10 @@
 //document.write("<script src='basic.js' type='text/javascript'></script>");
 
-function showdate(str){
+function getdate(str){
 	if (arguments.length==0)
 		{str=""}
 var date = new Date()      //e.g. 2012-12-20
-        document.write(str+date.toString())
-        //document.close()
+        return str + date.toString()
 }
 
 function showprop(obj){
