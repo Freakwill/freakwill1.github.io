48a0c8eb2d20caa1265f2bcdcf403e77236634ee
William Song <30965609+Freakwill@users.noreply.github.com>
2018-12-16 16:37:14 +0800

    28th commit

--- demo.js
+++ demo.js
@@ -30,4 +30,4 @@ function showprop(obj){
  */
 function showMatrix(q, width, height){
     var w=q.width=width,h=q.height=height, p=Array(256).join(1).split(''), c=q.getContext("2d");
-    setInterval(function(){c.fillStyle="rgba(0,0,0,0.05)";c.fillRect(0,0,w,h);c.fillStyle="rgba(0,255,0,1)";p=p.map(function(v,i){r=Math.random();c.fillText(String.fromCharCode(Math.floor(2720+r*33)),i*10,v);v+=10; return v>768+r*1e4?0:v})},33)}
+    return setInterval(function(){c.fillStyle="rgba(190,190,190,0.05)"; c.fillRect(0,0,w,h); c.fillStyle="rgba(0,255,0,1)"; p=p.map(function(v,i){r=Math.random();c.fillText(String.fromCharCode(Math.floor(2720+r*33)),i*10,v);v+=10; return v>768+r*1e4?0:v})},33)}
