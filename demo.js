0529a6e5e47574044859bbbc9cd616c50bd7f9c9
William Song <30965609+Freakwill@users.noreply.github.com>
2019-01-04 16:54:36 +0800

    ny3 commit

--- demo.js
+++ demo.js
@@ -2,11 +2,21 @@
  * Demonstration
  */
 
+
+module.exports = {
+    choice,
+    randomFromFile
+}
+
+
+var jquery = require("jquery")
+
 /**
  * get the date
  * @param  {String} str words befor date
  * @return {String}
  */
+
 function getdate(str){
 	if (arguments.length==0)
 		{str="";}
@@ -31,3 +41,5 @@ function showprop(obj){
 function showMatrix(q, width, height){
     var w=q.width=width,h=q.height=height, p=Array(256).join(1).split(''), c=q.getContext("2d");
     return setInterval(function(){c.fillStyle="rgba(190,190,190,0.05)"; c.fillRect(0,0,w,h); c.fillStyle="rgba(0,255,0,1)"; p=p.map(function(v,i){r=Math.random();c.fillText(String.fromCharCode(Math.floor(2720+r*33)),i*10,v);v+=10; return v>768+r*1e4?0:v})},33)}
+
+
